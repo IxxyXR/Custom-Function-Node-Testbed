812559db43ba1f6e14912eb24299ef6697e0c472
andybak <andy@andybak.net>
2019-09-21 09:31:46 +0100

    Actually add the shaders from http://halisavakis.com

---
+++ Assets/website-archive/BLUK-visuals/SkyColorManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class SkyColorManager : MonoBehaviour {
+  
+    public Color skyColor;
+    public Material fogMat;
+  
+    void Update () {
+        Camera.main.backgroundColor = skyColor;
+        fogMat.SetColor("_SkyColor", skyColor);
+    }
+}
